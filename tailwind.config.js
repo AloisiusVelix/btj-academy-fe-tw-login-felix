c9d09aaf51a456fe8abbf8048272484756b06788
AloisiusVelix <alogonzaga31@gmail.com>
2023-12-20 08:18:36 +0700

    Upload project login page with tailwind css framework

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: 'F28705',
+          50: '#FEF3E6',
+          500: '#F28705',
+          600: '#DC7B05'
+        },
+        black: {
+          DEFAULT: '#2A2A2A'
+        },
+        gray: {
+          10:'#1C1C1C',
+          custom: '#C0C0C0'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
